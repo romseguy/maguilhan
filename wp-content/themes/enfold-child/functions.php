e8f01d3535d59ab15852f1ef253cdd347afce527
Romain Séguy <rom.seguy@gmail.com>
2019-12-20 01:59:50 +0100

    album panier

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -4,3 +4,28 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
+
+/**
+ * Automatically add product to cart on visit
+ */
+add_action( 'template_redirect', 'add_product_to_cart' );
+function add_product_to_cart() {
+	if ( ! is_admin() ) {
+		$product_id = 19; //replace with your own product id
+		$found = false;
+		//check if product already in cart
+		if ( sizeof( WC()->cart->get_cart() ) > 0 ) {
+			foreach ( WC()->cart->get_cart() as $cart_item_key => $values ) {
+				$_product = $values['data'];
+				if ( $_product->get_id() == $product_id )
+					$found = true;
+			}
+			// if product not found, add it
+			if ( ! $found )
+				WC()->cart->add_to_cart( $product_id );
+		} else {
+			// if no products in cart, add it
+			WC()->cart->add_to_cart( $product_id );
+		}
+	}
+}
