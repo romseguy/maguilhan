ce51dfeac23d131d028c54126bee8e066c16d7d7
Romain Séguy <rom.seguy@gmail.com>
2019-12-24 12:33:56 +0100

    deploy

---
+++ deploy.sql
@@ -0,0 +1,12 @@
+UPDATE `wp_options` SET
+`option_id` = '1',
+`option_name` = 'siteurl',
+`option_value` = 'http://178.62.242.169/maguilhan',
+`autoload` = 'yes'
+WHERE `option_id` = '1';
+UPDATE `wp_options` SET
+`option_id` = '2',
+`option_name` = 'home',
+`option_value` = 'http://178.62.242.169/maguilhan',
+`autoload` = 'yes'
+WHERE `option_id` = '2';
